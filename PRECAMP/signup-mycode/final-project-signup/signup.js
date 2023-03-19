05371e17abcb90cc22215ed50088691ba8340533
xudegloss <ajouunihospitalrn@naver.com>
2023-03-20 03:03:19 +0900

    코드캠프 회원가입 인증번호 알림창 해결하기

--- PRECAMP/signup-mycode/final-project-signup/signup.js
+++ PRECAMP/signup-mycode/final-project-signup/signup.js
@@ -1,9 +1,4 @@
 // 1. 핸드폰 번호 작성 시에 알아서 옆으로 커서 넘어가도록 만들기.
-// 2. 비밀 번호와 비밀 번호 확인이 일치하는 경우에 인증번호 전송 가능해지기.
-// 3. 전송 누르면 인증 번호가 화면에 표시되고, 3분 카운트가 시작된다. 그리고 인증 완료 버튼이 활성화된다.
-// 4. 3분이 지나면 인증 완료 버튼이 비활성화된다.
-
-// 1. 핸드폰 번호 작성 시에 알아서 옆으로 커서 넘어가도록 만들기.
 
 const moveCursorFirst = () => {
   const p1 = document.getElementById("p1");
@@ -21,13 +16,19 @@ const moveCursorSec = () => {
   }
 };
 
-// 아직 해결하지 못 함. 비밀번호 가지고 나와야 된다.
-const changeValue = (event) => {
-  let pw1 = event.target.value;
-  return pw1;
+// 2. 핸드폰 번호까지 모두 입력한 경우 인증번호 전송 버튼 활성화하기.
+
+const moveCursorLast = () => {
+  const sendBtn = document.getElementById("send__num");
+  if (document.getElementById("p3").value.length === 4) {
+    sendBtn.disabled = false;
+    sendBtn.style.color = "#0068ff";
+    sendBtn.style.backgroundColor = "white";
+  }
 };
 
-// 2. 비밀 번호와 비밀 번호 확인이 일치하는 경우에 인증번호 전송 가능해지기.
+// 3. 인증번호 전송 버튼 누르면 인증 완료 버튼도 활성화시키기.
+// 4. 비밀 번호와 비밀 번호 확인이 일치하는 경우에 인증번호 전송 가능해지기.
 let isStarted = false;
 
 const sendNum = () => {
@@ -35,9 +36,16 @@ const sendNum = () => {
   let time = 180;
   let isDisabled = document.getElementById("complete__btn");
 
+  // 5. 인증 완료 버튼 누르면 타이머 멈추고, 인증 완료 알림창 띄우기.
+  if ((document.getElementById("complete__btn").disabled = false)) {
+    isStarted = true;
+  }
+
   if (isStarted === false) {
     isStarted = true;
     isDisabled.removeAttribute("disabled"); // 비활성화 끄기.
+    isDisabled.style.backgroundColor = "#0068ff";
+    isDisabled.style.color = "white";
 
     // 1. 6자리 수의 인증 번호 생성하고 화면에 보여주기.
     let certificateNum = document.getElementById("certificate__num");
@@ -65,5 +73,21 @@ const sendNum = () => {
       }
     }, 1000);
   } else {
+    // isStarted = true
+    alert("인증이 완료되었습니다.");
+    clearInterval(2);
+    // timer가 2이기 때문이다.
+    // timer 자체를 가져오고 싶었는데 자꾸 undefined가 떠서 timer가 반환하는 값을 넣어줬다.
   }
+};
+
+// 6. 가입 버튼 누르면 위의 빈칸을 모두 채웠는지 검토하고, 다 채운 경우에는 가입을 축하한다는 메세지를 띄우기.
+const checkAll = () => {
+  const email = document.getElementById("email").value;
+  const name = document.getElementById("name").value;
+  const pw1 = document.getElementById("pw1").value;
+  const pw2 = document.getElementById("pw2").value;
+  const p1 = document.getElementById("p1").value;
+  const p2 = document.getElementById("p2").value;
+  const p3 = document.getElementById("p3").value;
 };
