ba58e21657c0be8253ef309e5600b9b2c991c098
xudegloss <ajouunihospitalrn@naver.com>
2023-03-31 00:00:13 +0900

    함수 반환과 선언 종류

--- vailla-javascript-code/class/d-day-counter/app.js
+++ vailla-javascript-code/class/d-day-counter/app.js
@@ -19,19 +19,24 @@ const dateFormMaker = function () {
   const inputDate = document.querySelector("#target__date__input").value;
 
   // Template literals
+  // 또는 덧셈 연산자를 이용하는 경우도 있다. 상황에 맞게 더 편리한 것 이용하기.
 
   const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
   // 또 다른 함수에 dateFormat을 참조해야 한다.
   // 함수에 갇혀있는 변수 : 지역 변수
   // 함수 밖에 있는 변수 : 전역 변수
   // 바깥으로 나갈 수는 없지만, 안으로 들어갈 수 있다.
+
+  return dateFormat;
 };
 
 // 2.  countMaker 함수 만들기 : 현 시점에서 원하는 날짜까지 얼마나 남았는지 체크하는 함수 만들기.
 
 const countMaker = function () {
+  // return data
+  const targetDateInput = dateFormMaker();
   const newDate = new Date(); // 현 시점
-  const targetDate = new Date(dateFormat); // 원하는 날짜
+  const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0); // 원하는 날짜 (자정으로 설정하기.)
   const remaining = (targetDate - newDate) / 1000; // 원하는 날짜에서 현 시점까지 남은 s(초)
 
   // 남은 날짜, 남은 시간, 남은 분, 남은 초 나타내기.
---
+++ vailla-javascript-code/class/practice/function-return.js
@@ -0,0 +1,8 @@
+const sum = function () {
+  let result = 10 + 10;
+  return result;
+  console.log("함수가 종료되었습니다.");
+};
+// undefined
+sum();
+// 20
