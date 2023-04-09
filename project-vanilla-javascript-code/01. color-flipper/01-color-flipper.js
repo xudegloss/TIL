38b4e35fb942357ced4231d7f93ca84b65a4c577
xudegloss <ajouunihospitalrn@naver.com>
2023-04-09 20:15:11 +0900

    color flipper 기능 구현 완료하기

--- project-vanilla-javascript-code/01. color-flipper/01-color-flipper.js
+++ project-vanilla-javascript-code/01. color-flipper/01-color-flipper.js
@@ -1,19 +1,9 @@
-// 1. 버튼을 클릭했을 때, 색상이 랜덤으로 변경되는 함수
+let iframeSrc = document.getElementById("iframe");
 
-const changeBackGround = () => {
-  let colorArray = ["rgba(133, 122, 200)", "red", "green", "#f15025"];
-  let randomColor = Math.floor(Math.random() * colorArray.length);
+const leftBtnContainer = () => {
+  iframeSrc.src = "./main-header-left.html";
+};
 
-  const btn = document.querySelector(".clickBtn");
-  const container = document.getElementById("container");
-
-  container.style.backgroundColor = colorArray[randomColor];
-  btn.style.backgroundColor = colorArray[randomColor];
-
-  // 2. 마우스를 위로 올렸을 때, 배경색 검은색으로 변경되도록 만듦
-
-  btn.addEventListener(
-    "mouseover",
-    () => (btn.style.backgroundColor = "black")
-  );
+const rightBtnContainer = () => {
+  iframeSrc.src = "./main-header-right.html";
 };
