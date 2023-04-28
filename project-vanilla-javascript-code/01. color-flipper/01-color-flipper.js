21fc6c9b6dbacb0250fa16d86960159e6e3f5f94
xudegloss <ajouunihospitalrn@naver.com>
2023-04-28 13:37:59 +0900

    폴더 변경하기

--- project-vanilla-javascript-code/01. color-flipper/01-color-flipper.js
+++
@@ -1,9 +0,0 @@
-let iframeSrc = document.getElementById("iframe");
-
-const leftBtnContainer = () => {
-  iframeSrc.src = "./main-header-left.html";
-};
-
-const rightBtnContainer = () => {
-  iframeSrc.src = "./main-header-right.html";
-};
